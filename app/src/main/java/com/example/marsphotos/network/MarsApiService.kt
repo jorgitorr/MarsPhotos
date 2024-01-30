3c6ec9bf888c1a5b4bad19b4699754ea29d6e634
jorgitorr <jorgitorr97@gmail.com>
2024-01-30 23:45:55 +0100

    añadido interfaz en el archivo MarsApiService y las variables BASE_URL y retrofit que tiene el enlace a la api y se le pasa un conversor para convertir el JSON a String.

--- app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
+++ app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
@@ -1,4 +1,18 @@
 package com.example.marsphotos.network
 
-class MarsApiService {
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+
+private const val BASE_URL =
+    "https://android-kotlin-fun-mars-server.appspot.com"
+
+
+//agrega compilador de Retrofit para compilar y crear un objeto Retrofit
+//Retrofit requiere el URI base para el servicio web y una fábrica de conversión para crear una API de servicios web.
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(ScalarsConverterFactory.create())//añadimos el conversor
+    .baseUrl(BASE_URL)//se le pasa la url base de la API
+    .build()//crea el objeto Retrofit
+
+interface MarsApiService {
 }
